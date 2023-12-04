5795b2b0c46aa0da3a42248c0171cd13f8a06bca
Johannes Blaschke <jpblaschke@lbl.gov>
2023-12-04 15:53:55 -0800

    wip on harder problem from day 1

--- day-1/src/main.rs
+++ day-1/src/main.rs
@@ -1,5 +1,59 @@
+use core::num;
 use std::fs;
 use std::path::{Path, PathBuf};
+// use std::sync::Mutex;
+use std::cmp::min;
+
+#[macro_use]
+extern crate lazy_static;
+
+lazy_static! {
+    static ref TOKENS: Vec<&'static str> = {
+        let tokens_1 = [
+                "zero", "one", "two", "three", "four", "five", "six", "seven",
+                "eight", "nine"
+            ];
+        let mut tokens_2: Vec<&str> = Vec::new();
+        for t in tokens_1 {
+            tokens_2.push(t);
+        }
+        tokens_2
+    };
+    static ref OFFSETS: Vec<usize> = {
+        let mut offsets: Vec<usize> =  Vec::new();
+        for t in TOKENS.iter() {
+            offsets.push(t.len());
+        }
+        offsets
+    };
+}
+
+fn match_tokens(string: &str) -> Vec<char> {
+    let mut number_chars: Vec<char> = Vec::new();
+
+    let mut char_iter = string.chars().enumerate().into_iter();
+    while let Some((i, c)) = char_iter.next() {
+        // println!("Testing: {} {}", i, c);
+        if c.is_numeric() {
+            number_chars.push(c);
+            continue;
+        }
+
+        for (ti, t) in TOKENS.iter().enumerate() {
+            if string[i..].starts_with(t) {
+                // println!("i={}, token={}", i, ti);
+                number_chars.push(char::from_digit(ti as u32, 10).unwrap());
+
+                // Skip ahead by the length of the matched token
+                char_iter.by_ref().take(OFFSETS[ti] - 1).for_each(drop);
+                break;
+            }
+        }
+    }
+ 
+    return number_chars;
+}
+
 
 fn solution_1(input_file:PathBuf) -> i64 {
     let input = fs::read_to_string(input_file).expect(
@@ -8,12 +62,15 @@ fn solution_1(input_file:PathBuf) -> i64 {
 
     let mut numbers: Vec<i64> = Vec::new();
     for line in input.split("\n") {
-        let mut chars: Vec<char> = Vec::new();
-        for c in line.chars() {
-            if c.is_numeric() {
-                chars.push(c);
-            }
-        }
+        //let mut chars: Vec<char> = Vec::new();
+        // for c in line.chars() {
+        //     if c.is_numeric() {
+        //         chars.push(c);
+        //     }
+        // }
+
+        let chars = match_tokens(line);
+
         if chars.len() > 0 {
             let mut valid_digits: Vec<char> = Vec::new();
             valid_digits.push(* chars.first().unwrap());
@@ -38,6 +95,10 @@ fn main() {
 
     let answer = solution_1(problem_1_input);
     println!("The answer to problem 1 is: {}", answer);
+
+    let vd = match_tokens("two1nine");
+    let vd_str: String = vd.iter().collect();
+    println!("{}", vd_str);
 }
 
 
